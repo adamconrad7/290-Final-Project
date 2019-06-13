da767362f6da321a6d3c1836145e64dbd7808f78
reidashley <ashleycreid3@gmail.com>
2019-06-13 02:00:57 -0700

    temp files added

---
+++ public/tempIndex.js
@@ -0,0 +1,247 @@
+// Gets username of favorited user and stores them in array:
+var buttons = document.getElementsByClassName('fav-button');
+
+var favoritedUsers = [' '];
+
+var addFav = function() {
+    var button = this.firstChild.nextSibling;
+    button.classList.add('pressed');
+    var username = this.parentNode.innerText;
+    username = username.substring(0, username.indexOf('\n'));
+    //loops over list of favorited users to ensure that no duplicates are added:
+    for(var i=0; i<favoritedUsers.length; i++){
+      if(favoritedUsers[i] !== username ){
+        if(i === favoritedUsers.length-1){
+            favoritedUsers.push(username);
+            var postRequest = new XMLHttpRequest();
+            var requestURL = '/addFav';
+            postRequest.open('POST', requestURL);
+            // var requestBody = JSON.stringify({
+            //   favsList: favoritedUsers
+            // });
+            var requestBody = JSON.stringify({
+              favUser: username
+            });
+
+            postRequest.addEventListener('load', function(event){
+              if (event.target.status === 200) {
+
+              }
+            });
+
+            button.classList.remove('pressed');
+
+            postRequest.setRequestHeader('Content-Type', 'application/json');
+            postRequest.send(requestBody);
+            break;
+        }
+      }/*else{
+        //If user exists in favorites array, remove them and make button un pressed:
+        favoritedUsers.splice(i,1);
+        button.classList.remove('pressed');
+        break;
+      }
+      */
+    }
+};
+
+for(var i=0; i<buttons.length; i++){
+    buttons[i].addEventListener('click', addFav, false);
+}
+
+//These two functions display and hide the login modal, respectively:
+var displayModal1 = function(){
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var loginModal = document.getElementById('login-modal');
+  modalBackdrop.classList.remove('hidden');
+  loginModal.classList.remove('hidden');
+}
+
+var hideModal1 = function(){
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var loginModal = document.getElementById('login-modal');
+  modalBackdrop.classList.add('hidden');
+  loginModal.classList.add('hidden');
+}
+
+var displayModal2 = function(){
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var loginModal = document.getElementById('sign-up-modal');
+  modalBackdrop.classList.remove('hidden');
+  loginModal.classList.remove('hidden');
+}
+
+var hideModal2 = function(){
+  var modalBackdrop = document.getElementById('modal-backdrop');
+  var loginModal = document.getElementById('sign-up-modal');
+  modalBackdrop.classList.add('hidden');
+  loginModal.classList.add('hidden');
+}
+
+//Global array containing all users:
+
+
+//Gets username from login:
+var getUsername = function(){
+  var currentUsername = document.getElementById('login-text-input').value;
+  console.log(currentUsername);
+  //Make sure user entered name:
+  if(currentUsername){
+    //loop over all stored names:
+    for(var i=0; i<allUsers.length; i++){
+      //if user name doesnt match any stored ones, store it:
+      if(allUsers[i] !== currentUsername ){
+        if(i === allUsers.length-1){
+          //Here we need to bring the new user to a create profile modal where they fill in bio fields, add photos, etc
+          allUsers.push(currentUsername);
+          hideModal1();
+          /*displayModal2(); */     //just added
+          break;
+        }
+      }else {
+        //Here we need to load existing user's data such as profile fields and favortied users list.
+        alert('Username is taken.');
+        break;
+      }
+    }
+  }else{
+    alert("Please specify a username.");
+  }
+}
+
+// var postRequest = new XMLHttpRequest();
+// var requestURL = '/addFav';
+// postRequest.open('POST', requestURL);
+// var requestBody = JSON.stringify({
+//   favsList: favoritedUsers
+// });
+// postRequest.addEventListener('load', function(event){
+//   if (event.target.status === 200) {
+//
+//   }
+// });
+// postRequest.setRequestHeader('Content-Type', 'application/json');
+// postRequest.send(requestBody);
+
+var enterButton = document.getElementsByClassName('modal-accept-button')[0];
+enterButton.addEventListener('click', getUsername);
+
+var loginButton = document.getElementsByClassName('login-button')[0];
+loginButton.addEventListener('click', displayModal1);
+
+window.addEventListener('DOMContentLoaded', function () {
+  //login modal is displayed upon page load:
+  console.log("dom content loaded")
+  // displayModal1();
+});
+
+
+var favButton = document.getElementsByClassName('favorite')[0];
+favButton.addEventListener('click',function(){
+  displayFavs();
+})
+
+var everyoneButton = document.getElementsByClassName('everyone')[0];
+everyoneButton.addEventListener('click',function(){
+  showAllUsers();
+})
+
+var displayFavs = function(){
+  allPeople = document.getElementsByClassName('user-card');
+  for(var i =0; i<allPeople.length; i++){
+    var username = allPeople[i].innerText;
+    username = username.substring(0, username.indexOf('\n'));
+    if(!checkFavs(username)){
+      // console.log(username, " is not a favorite.");
+      allPeople[i].classList.add('hidden');
+    }
+  }
+}
+
+var checkFavs = function(name){
+  for(var i=0; i<favoritedUsers.length; i++){
+    if(name === favoritedUsers[i]){
+      return true;
+    }else{
+      if(i === favoritedUsers.length-1){
+        return false;
+      }
+    }
+  }
+}
+
+var removeFav = function() {
+  var button = this.firstChild.nextSibling;
+  button.classList.remove('pressed');
+  var username = this.parentNode.innerText;
+  username = username.substring(0, username.indexOf('\n'));
+  //loops over list of favorited users to ensure that no duplicates are added:
+  for(var i=0; i<favoritedUsers.length; i++){
+    if(favoritedUsers[i] !== username ){
+      if(i === favoritedUsers.length-1){
+          favoritedUsers.pop(username);
+          var postRequest = new XMLHttpRequest();
+          var requestURL = '/removeFav';
+          postRequest.open('POST', requestURL);
+          // var requestBody = JSON.stringify({
+          //   favsList: favoritedUsers
+          // });
+
+          var requestBody = JSON.stringify({
+            favUser: username
+          });
+
+          postRequest.addEventListener('load', function(event){
+            if (event.target.status === 200) {
+
+            }
+          });
+          postRequest.setRequestHeader('Content-Type', 'application/json');
+          postRequest.send(requestBody);
+          break;
+      }
+    }else{
+      //If user exists in favorites array, remove them and make button un pressed:
+      favoritedUsers.splice(i,1);
+      button.classList.remove('pressed');
+      break;
+    }
+  }
+};
+
+var showAllUsers = function(){
+  allPeople = document.getElementsByClassName('user-card');
+  for(var i =0; i<allPeople.length; i++){
+      allPeople[i].classList.remove('hidden');
+  }
+}
+
+var allusers = document.getElementsByClassName('users-container')[0].children;
+// userName = allusers[0].innerText.trim();
+// userName = userName.substring(0, userName.indexOf('\n'));
+// // userName = userName.innerText;
+// // userName = userName.trim();
+// console.log(userName);
+// userName = userName.substring(0, userName.indexOf('\n'));
+// console.log(userName);
+var allUsers = [];
+
+for(var i=0; i<allusers.length-3; i++){
+   userName = allusers[i].innerText.trim();
+   userName = userName.substring(0, userName.indexOf('\n'));
+   allUsers.push(userName);
+}
+
+console.log(allUsers);
+
+//
+// var setActive = function(classname){
+//   classname.classList.add('active');
+// }
+//
+// var removeActive = function(classname){
+//   classname.classList.remove('active');
+// }
+//
+// var navlink = document.getElementsByClassName
+// navlink.addEventListener('click', setActive(navlink));
---
+++ tempServer.js
@@ -0,0 +1,196 @@
+
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+
+var app = express();
+var port = process.env.PORT || 8000;
+
+var mongoHost = 'classmongo.engr.oregonstate.edu';
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = 'cs290_reidash';
+var mongoPassword =  'cs290_reidash';
+var mongoDBName = 'cs290_reidash';
+
+// mongoimport --host classmongo.engr.oregonstate.edu \
+//   --username cs290_reidash                 \
+//   --db cs290_reidash                        \
+//   --password cs290_reidash                \
+//   --collection users --jsonArray userData.json
+
+// mongoimport --host classmongo.engr.oregonstate.edu \
+//   --username cs290_reidash                 \
+//   --db cs290_reidash                        \
+//   --password cs290_reidash                \
+//   --collection favArray --file favArray.json
+
+// mongo -u cs290_reidash -p cs290_reidash \
+//     classmongo.engr.oregonstate.edu/cs290_reidash
+
+
+//var mongoUrl = `mongodb://@${mongoHost}:${mongoPort}/db`;
+var mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDBName}`;
+
+var db = null;
+
+//var usersArr = require('./userData.json');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}) );
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+// app.get('/', function(req, res, next) {
+//     console.log('responding to request for root');
+//     var users = db.collection("users");
+//     var usersCursor = users.find({});
+//     usersCursor.toArray(function (err, usersArr){
+//       if(err){
+//         throw err;
+//         res.status(500).send({
+//           error: "Error fetching users from DB"
+//         });
+//       }else{
+//         console.log("Fetching users from database", users);
+//         res.send(usersArr);
+//         next();
+//       }
+//     });
+// });
+
+//Catches root path and serves all twits:
+app.get('/', function (req, res) {
+  console.log('responding to request for root');
+  var users = db.collection("users");
+  //console.log("Count is: ", users.find({}).count());
+  var usersCursor = users.find({});
+  usersCursor.toArray(function (err, usersArr){
+    if(err){
+      throw err;
+      res.status(500).send({
+        error: "Error fetching users from DB"
+      });
+    }else{
+      // console.log("Fetching users from database", users);
+      var usersString = JSON.stringify(usersArr);
+      // res.write(usersString);
+      res.render('browsePage', {
+        users: usersArr,
+      });
+    }
+  });
+});
+
+
+//serves favorites
+app.get('/favs', function(req, res) {
+    console.log('responding to request for favs');
+    var users = db.collection("users");
+  //  var users = db.users.find( );
+
+  //ned to make this flip bool:
+    var usersCursor = users.find({fav: true});
+    usersCursor.toArray(function (err, usersArr){
+      if(err){
+        throw err;
+        res.status(500).send({
+          error: "Error fetching users from DB"
+        });
+      }else{
+        console.log("Fetching users from database", users);
+        res.render('browsePage', {
+          users: usersArr,
+        });
+      }
+    });
+});
+
+
+app.get('/user', function (req, res) {
+  console.log('responding to request for bio page');
+    res.render('bioPage', {
+
+    });
+});
+
+//render browsepage but only with users who have true bool:
+app.get('/favs',function(req, res){
+
+});
+
+//this needs to recive a name and change the bool of that name to true:
+app.post('/addFav', function (req, res){
+  console.log("got a post request");
+  if (req.body && req.body.favUser) {
+    console.log("== Client added the following users to their favorites list:");
+    console.log("  - favsList:", req.body.favsList);
+  //  var favsArray = db.collection("favsArray");
+
+    var users = db.collection("users");
+    // var favoritesList = {
+    //   favorites: req.body.favUser
+    // };
+    var username = req.body.favUser;
+    console.log(username);
+    //search userarray for name and set bool to true:
+    users.updateOne(
+      { name: username },
+      { $set: { fav: true}
+    });
+    // console.log('\n','\n','\n','\n');
+    // console.log(favoritesList);
+    // debugger;
+    // favsArray.updateOne(
+    //   {"_id": ObjectId("5d0184ea73e6c6016cb65c22")},
+    //   { $set: favoritesList}
+    // );
+// db.artists.insert({ artistname: "The Tea Party" })
+    res.status(200).send("favsList successfully added");
+  } else {
+    res.status(400).send("Requests to this path must " +
+      "contain a JSON body with photoURL and caption " +
+      "fields.");
+  }
+});
+
+
+MongoClient.connect(mongoUrl, function (err, client) {
+  if (err) {
+    throw err;
+  }
+  db = client.db(mongoDBName);
+  app.listen(port, function () {
+    console.log("== Server listening on port", port);
+  });
+});
+
+
+app.post('/removeFav', function (req, res){
+  console.log("got a post request");
+  if (req.body && req.body.favUser) {
+    console.log("== Client added the following users to their favorites list:");
+    console.log("  - favsList:", req.body.favsList);
+  //  var favsArray = db.collection("favsArray");
+
+    var users = db.collection("users");
+    // var favoritesList = {
+    //   favorites: req.body.favUser
+    // };
+    var username = req.body.favUser;
+    console.log(username);
+    //search userarray for name and set bool to true:
+    users.updateOne(
+      { name: username },
+      { $set: { fav: false}
+    });
+    res.status(200).send("removeList successfully removed");
+  } else {
+    res.status(400).send("Requests to this path must " +
+      "contain a JSON body with photoURL and caption " +
+      "fields.");
+  }
+});
